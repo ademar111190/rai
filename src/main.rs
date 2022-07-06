4333119c33254107dbbe9911ad8688cabea1054e
Ademar Alves de Oliveira <ademar111190@gmail.com>
2022-07-06 10:55:00 -0300

    Add a request response method working (hello world 2.0)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use std::time::{SystemTime, UNIX_EPOCH};
+
+struct PayloadRequest {
+    platform: String,
+}
+
+struct PayloadResponse {
+    message: String,
+    timestamp: u64,
+}
+
+fn hello_world(request: PayloadRequest) -> PayloadResponse {
+    return PayloadResponse {
+        message: format!("Hello World from Rust to {}!", request.platform),
+        timestamp: SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .expect("Time went backwards")
+            .as_secs(),
+    };
+}
+
 fn main() {
-    println!("Hello, world!");
+    let response = hello_world(PayloadRequest {
+        platform: String::from("Rust"),
+    });
+    println!("{}", response.message);
+    println!("Timestamp: {}", response.timestamp);
 }
