f395b402e3c0559115034df4d7106d53af7c87a0
Ademar Alves de Oliveira <ademar111190@gmail.com>
2022-07-07 11:33:14 -0300

    Uses coroutine instead of thread

--- android/app/src/main/java/ademar/rai/MainActivity.kt
+++ android/app/src/main/java/ademar/rai/MainActivity.kt
@@ -4,6 +4,10 @@ import ademar.rai.lib.Rai
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,18 +17,20 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private val scope = CoroutineScope(Dispatchers.Main)
     private val txt: TextView
         get() = findViewById(R.id.txt)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Thread {
-            val result = Rai.helloWorldSync("Android")
-            runOnUiThread {
-                txt.text = result
-            }
-        }.start()
+        scope.launch {
+            txt.text = communicateToRust()
+        }
+    }
+
+    private suspend fun communicateToRust(): String = withContext(Dispatchers.IO) {
+        Rai.helloWorldSync("Android")
     }
 
 }
